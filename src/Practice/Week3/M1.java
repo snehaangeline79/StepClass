8cae5a57999997838e177d89754fb72d4c3dc548
Sneha <sneha.jeejee@gmail.com>
2026-09-19 16:37:38 +0530

    Add Week 3 programs

---
+++ src/Practice/Week3/M1.java
@@ -0,0 +1,31 @@
+package week3;
+class PlacementRecord {
+    String studentName;
+    String company;
+    double packageLpa;
+
+    PlacementRecord(String studentName, String company, double packageLpa) {
+        this.studentName = studentName;
+        this.company = company;
+        this.packageLpa = packageLpa;
+    }
+
+    void printRecord() {
+        System.out.println(studentName + " -> " + company + " @ " + packageLpa + " LPA");
+    }
+}
+
+public class M1 {
+    public static void main(String[] args) {
+
+        PlacementRecord[] records = {
+                new PlacementRecord("Ravi", "TCS", 4.5),
+                new PlacementRecord("Anitha", "Zoho", 6.2),
+                new PlacementRecord("Karthik", "Infosys", 4.0)
+        };
+
+        for (PlacementRecord record : records) {
+            record.printRecord();
+        }
+    }
+}
